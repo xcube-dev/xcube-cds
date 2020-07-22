50af9e4623ea5a87b1e2703f774f77a76f039a42
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2020-07-22 13:33:46 +0200

    Add script to make variable descriptors from NetCDF
    Add a script which reads a NetCDF file and outputs Python source code
    which creates VariableDescriptor objects corresponding to the
    variables in the NetCDF file.

---
+++ misc/create_variable_descriptors_from_netcdf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""Create variable descriptor source code from a NetCDF file.
+
+An xcube data store plugin must implement a describe_data method returning
+a DataDescriptor (which in turn contains VariableDescriptors) for each
+supported data ID. This script takes a NetCDF file as its first and only
+argument and prints Python source code to create corresponding
+VariableDescriptors.
+"""
+
+import sys
+from netCDF4 import Dataset
+
+
+def main():
+    with Dataset(sys.argv[1], 'r') as ds:
+        for vname, vinfo in ds.variables.items():
+            if '_CoordinateAxisType' not in vinfo.ncattrs():
+                output_variable(vname, vinfo)
+
+
+class VariableDescriptor:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def constructor_string(self):
+        output = []
+        for arg, value in self.kwargs.items():
+            output.append(f"{arg}={value!r}")
+        return 'VariableDescriptor(\n    ' + ',\n    '.join(output) + '\n),\n'
+
+
+def output_variable(vname, vinfo):
+    attr_map = {}
+    for attr_name in 'units', 'long_name':
+        if attr_name in vinfo.ncattrs():
+            attr_map[attr_name] = vinfo.getncattr(attr_name)
+
+    vd = VariableDescriptor(name=vname, dtype=vinfo.datatype,
+                            dims=vinfo.dimensions,
+                            attrs=attr_map)
+    print(vd.constructor_string())
+
+
+if __name__ == '__main__':
+    main()
