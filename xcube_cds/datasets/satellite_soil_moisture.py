14d28d470d831a863faa6942a71e92f27de4b8ae
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 19:17:46 +0000

    Adding tarfile member sanitization to extractall()

--- xcube_cds/datasets/satellite_soil_moisture.py
+++ xcube_cds/datasets/satellite_soil_moisture.py
@@ -134,7 +134,26 @@ class SoilMoistureHandler(CDSDatasetHandler):
                   cds_api_params: Dict, file_path: str, temp_dir: str):
         # Unpack the .tar.gz into the temporary directory.
         with tarfile.open(file_path) as tgz_file:
-            tgz_file.extractall(path=temp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz_file, path=temp_dir)
 
         paths = [os.path.join(temp_dir, filename) for filename in
                  next(os.walk(temp_dir))[2]]
