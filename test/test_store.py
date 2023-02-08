b40e7ff507504b1a8cbf134475d8a4fc38e65af4
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2023-02-08 17:11:20 +0100

    Update version unit test to pass any semver string

--- test/test_store.py
+++ test/test_store.py
@@ -249,7 +249,15 @@ class CDSStoreTest(unittest.TestCase):
             )
 
     def test_version_number(self):
-        valid_version = re.compile(r'^(\d[.])+(dev\d+|\d+)$')
+        # The official semver regex, from
+        # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+        valid_version = re.compile(
+            r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.'
+            r'(?P<patch>0|[1-9]\d*)'
+            r'(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
+            r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
+            r'(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+        )
         import xcube_cds.version
         self.assertTrue(valid_version.match(xcube_cds.version))
         self.assertTrue(valid_version.match(xcube_cds.__version__))
