9be4593b23727a0fb70cb1fdb52124fc8f176522
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2023-05-05 10:05:32 +0200

    Improve CDS client mocking
    URL and key parameters were not being correctly handled in all cases.

--- test/mocks.py
+++ test/mocks.py
@@ -21,8 +21,25 @@ class _SessionMock:
         pass
 
 
-class CDSClientMock:
+def _get_url_and_key(url, key):
+    if url is None:
+        url = os.environ.get('CDSAPI_URL')
+    if key is None:
+        key = os.environ.get('CDSAPI_KEY')
+    dotrc = os.environ.get('CDSAPI_RC', os.path.expanduser('~/.cdsapirc'))
+    if url is None or key is None:
+        if os.path.exists(dotrc):
+            config = cdsapi.api.read_config(dotrc)
+            if key is None:
+                key = config.get('key')
+            if url is None:
+                url = config.get('url')
+    if url is None or key is None:
+        raise Exception(f'Missing/incomplete configuration file: {dotrc}')
+    return url, key
 
+
+class CDSClientMock:
     """A simple mock of the cdsapi.Client class
 
     This mock class uses predefined requests from on-disk JSON files. When
@@ -43,27 +60,9 @@ class CDSClientMock:
 
     def __init__(self, url=None, key=None):
         self.session = _SessionMock()
+        self.url, self.key = _get_url_and_key(url, key)
 
-        if url is None:
-            url = os.environ.get('CDSAPI_URL')
-        if key is None:
-            key = os.environ.get('CDSAPI_KEY')
-        dotrc = os.environ.get('CDSAPI_RC', os.path.expanduser('~/.cdsapirc'))
-        if url is None or key is None:
-            if os.path.exists(dotrc):
-                config = cdsapi.api.read_config(dotrc)
-                if key is None:
-                    key = config.get('key')
-                if url is None:
-                    url = config.get('url')
-        if url is None or key is None:
-            raise Exception(f'Missing/incomplete configuration file: {dotrc}')
-
-        self.url = url
-        self.key = key
-
-        resource_path = os.path.join(os.path.dirname(__file__),
-                                     'mock_results')
+        resource_path = os.path.join(os.path.dirname(__file__), 'mock_results')
         # request_map is a list because dicts can't be hashed in Python, and
         # it's not worth introducing a dependency on frozendict just for this.
         self.request_map = []
@@ -71,7 +70,9 @@ class CDSClientMock:
             dir_path = os.path.join(resource_path, d)
             with open(os.path.join(dir_path, 'request.json'), 'r') as fh:
                 request = json.load(fh)
-            self.request_map.append((request, os.path.join(dir_path, 'result')))
+            self.request_map.append(
+                (request, os.path.join(dir_path, 'result'))
+            )
 
     def _get_result(self, request):
         for canned_request, canned_result in self.request_map:
@@ -80,42 +81,51 @@ class CDSClientMock:
         raise KeyError('Request not recognized')
 
     def retrieve(self, dataset_name, params, file_path):
-        params_with_name = {**dict(_dataset_name=dataset_name),
-                            **params}
+        params_with_name = {**dict(_dataset_name=dataset_name), **params}
         shutil.copy2(self._get_result(params_with_name), file_path)
+
+
+class CDSClientWrapper:
+    def __init__(self, url=None, key=None):
+        self.session = _SessionMock()
+        self.real_client = cdsapi.Client()
+        self.url, self.key = _get_url_and_key(url, key)
+
+    def retrieve(self, dataset_name, params, file_path):
+        self.real_client.retrieve(dataset_name, params, file_path)
 
 
 def get_cds_client(dirname=None):
     if _BEHAVIOUR is _Behaviour.MOCK:
         # Use pre-generated response data for known requests.
         return CDSClientMock
+
     elif _BEHAVIOUR is _Behaviour.REAL_CLIENT:
-        # Use the real cdsapi client.
-        return cdsapi.Client
+        # Use the real cdsapi client, but wrap it to ignore the passed-in
+        # credentials (which will probably be dummy values) and fall back
+        # to environment variables.
+        return CDSClientWrapper
+
     elif _BEHAVIOUR is _Behaviour.SAVE_RESULTS:
         # Wrap the real client and save the requests and responses
-        # for future mocking.
+        # for future mocking. As above, ignore passed-in credentials.
         if dirname is None:
+            # Default directory name is name of calling function.
             dirname = inspect.currentframe().f_back.f_code.co_name
-        resource_path = os.path.join(os.path.dirname(__file__),
-                                     'mock_results')
+        resource_path = os.path.join(os.path.dirname(__file__), 'mock_results')
         path = os.path.join(resource_path, dirname)
 
-        class CDSClientWrapper:
-
-            def __init__(self, url=None, key=None):
-                self.session = _SessionMock()
-                self.real_client = cdsapi.Client()
-
+        class ResultSavingClientWrapper(CDSClientWrapper):
             def retrieve(self, dataset_name, params, file_path):
-                params_with_name = {**dict(_dataset_name=dataset_name),
-                                    **params}
+                params_with_name = {
+                    **dict(_dataset_name=dataset_name),
+                    **params,
+                }
                 with open(os.path.join(path, 'request.json'), 'w') as fh:
                     json.dump(params_with_name, fh)
                 self.real_client.retrieve(dataset_name, params, file_path)
                 shutil.copy2(file_path, os.path.join(path, 'result'))
 
-        return CDSClientWrapper
+        return ResultSavingClientWrapper
     else:
         raise Exception(f'Unknown behaviour {_BEHAVIOUR}')
-
